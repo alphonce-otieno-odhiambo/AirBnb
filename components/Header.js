9fa3682199c925f7efa440835b5b49b71bab5149
alphonce-otieno-odhiambo <alphonce.odhiambo@student.moringaschool.com>
2022-03-14 23:28:38 +0300

    import date rangepiker

--- components/Header.js
+++ components/Header.js
@@ -10,8 +10,17 @@ import {
 import {useState} from 'react';
 import 'react-date-range/dist/styles.css'; // main style file
 import 'react-date-range/dist/theme/default.css'; // theme css file
+import { DateRangePicker } from 'react-date-range';
 function Header() {
-const [searchInput, setSearchInput] = useState("")
+const [searchInput, setSearchInput] = useState("");
+const [startDate,setStartDate ] = useState(new Date());
+const [endDate, setEndDate] = useState(new Date())
+
+const selectionRange = {
+  startDate :startDate,
+  endDate :endDate,
+  key :"Selection"
+}
 
   return (
     <header className=' sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10 '>
@@ -45,7 +54,11 @@ const [searchInput, setSearchInput] = useState("")
            
        </div>
        {searchInput && (
-       <div></div>
+       <div>
+         <DateRangePicker
+         ranges={[selectionRange]}
+         />
+       </div>
        )}
        
     </header>
