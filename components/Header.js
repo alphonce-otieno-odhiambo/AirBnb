b93d944fe3f43cde05fcd2df0a1846a43ecb1dba
alphonce-otieno-odhiambo <alphonce.odhiambo@student.moringaschool.com>
2022-03-14 13:33:48 +0300

    style the search input

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import Image from 'next/image'
+import {
+    SearchIcon,
+    
+} from '@heroicons/react/solid';
 function Header() {
   return (
-    <header className='sticky top-0 z-50 grid grid-col-3 bg-white shadow-md p-5 md:px-10'>
+    <header className=' sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10 '>
        
        {/* left */}
        <div className='relative  flex items-center h-10 cursor-pointer my-auto'>
@@ -14,8 +18,9 @@ function Header() {
            />
        </div>
        {/* middle */}
-       <div>
-           <input type="text" placeholder='Search'/>
+       <div className='flex items-center md:border-2 py-2 rounded-full md:shadow-sm'>
+           <input className='pl-5 bg-transparent outline-none flex-grow' type="text" placeholder='Search'/>
+           <SearchIcon className='hidden md:inline-flex h-8 bg-red-400 text-white md:mx-2 rounded-full cursor-pointer p-2'/>
        </div>
        {/* right */}
        <div></div>
