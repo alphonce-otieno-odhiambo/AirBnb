671d16438958967705c092266fa130c4fbb0fd26
alphonce-otieno-odhiambo <alphonce.odhiambo@student.moringaschool.com>
2022-03-14 14:19:34 +0300

    style text & btn

--- components/Banner.js
+++ components/Banner.js
@@ -11,6 +11,8 @@ function Banner() {
         />
         <div className='absolute top-1/2 w-full text-center'>
             <p className='text-sm sm:text-lg'>Not sure where to go</p>
+            <button 
+            className='text-purple-500 bg-white px-10 py-4 shadow-md rounded-full font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150'>I'm Flexible</button>
         </div>
     </div>
   )
