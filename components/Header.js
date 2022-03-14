c3921cf46de6ef9d80f4f3c73aee130dd9d5a813
alphonce-otieno-odhiambo <alphonce.odhiambo@student.moringaschool.com>
2022-03-14 12:54:47 +0300

    import airbnb logo

--- components/Header.js
+++ components/Header.js
@@ -5,10 +5,12 @@ function Header() {
     <header>
        
        {/* left */}
-       <div>
+       <div className='relative  flex items-center h-10 cursor-pointer my-auto'>
            <Image 
            src="https://links.papareact.com/qd3"
            layout="fill"
+           objectFit='contain'
+           objectPosition='left'
            />
        </div>
        {/* middle */}
