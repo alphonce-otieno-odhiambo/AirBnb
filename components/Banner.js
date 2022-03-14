ea0d958afd2e622e89891b9e7bc7a137a653f470
alphonce-otieno-odhiambo <alphonce.odhiambo@student.moringaschool.com>
2022-03-14 14:00:11 +0300

    create a banner fine

---
+++ components/Banner.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Image from 'next/image';
+
+function Banner() {
+  return (
+    <div>
+        <Image 
+        src='https://links.papareact.com/0fm'
+        layout='fill'
+        />
+    </div>
+  )
+}
+
+export default Banner
